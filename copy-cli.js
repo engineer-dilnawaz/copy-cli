9f1f261aeba23d653889e67d99232bf074e9214f
Dilnawaz Khan <dilnawaz.khan.engineer@gmail.com>
2026-01-07 01:26:18 +0500

    new changes

--- copy-cli.js
+++ copy-cli.js
@@ -1,25 +1,31 @@
 #!/usr/bin/env node
 
 import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname as pathDirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = pathDirname(__filename);
 
 const args = process.argv.slice(2);
-const dirname = import.meta.dirname;
-const filname = import.meta.filename;
 
 if (args.includes("--help")) {
   console.log(`Usage:
-        copy <source> <destination>
+  copy <source> <destination>
+
 Options:
-        --help      Show help
-        --version   Show version
-        `);
+  --help      Show help
+  --version   Show version
+`);
   process.exit(0);
 }
 
 if (args.includes("--version")) {
-  const packageJSON = await fs.readFile(`${dirname}/package.json`, "utf-8");
+  const packageJSON = await fs.readFile(
+    join(__dirname, "package.json"),
+    "utf-8"
+  );
   const version = JSON.parse(packageJSON).version;
-
   console.log(`copy v${version}`);
   process.exit(0);
 }
@@ -40,4 +46,4 @@ if (!destinationPath) {
 const imageBuffer = await fs.readFile(fileName);
 await fs.writeFile(`${destinationPath}/${fileName}`, imageBuffer);
 
-console.log("Image paste successfully");
+console.log("File copied successfully!");
